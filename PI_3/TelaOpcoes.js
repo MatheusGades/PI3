aa68ec32e8ca4c2ce82c21ec7c795de102d5ad9f
Matheus Gades <matheus.gcosta.81@gmail.com>
2023-11-20 08:59:22 -0300

    .

--- PI_3/TelaOpcoes.js
+++
@@ -1,9 +0,0 @@
-// Função que será executada quando o botão "Comprar" for clicado
-document.getElementById("comprar").addEventListener("click", function() {
-    window.location.href = "TelaFinalizacao.html"; // Redireciona para a página "TelaFinalizacao.html"
-});
-
-  // Função que será executada quando o botão "Sair" for clicado
-  document.getElementById("sair").addEventListener("click", function() {
-    window.close(); 
- });
